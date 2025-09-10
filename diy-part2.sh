53c0f9eb47e8b5406caa73f4137f5a727da86b08
Cyanro <113330384+Cyanro@users.noreply.github.com>
2025-09-10 22:13:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-l
 # 修改默认主机名
 # sed -i 's/LEDE/CyanTec/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/CyanTec/g' package/base-files/luci2/bin/config_generate
+
+#修改JCGQ20的nand
+sed -i '/&nand\s*{/{:a;n;/status\s*=\s*"okay";/{a\
+\tnand-ecc-step-size = <512>;\
+\tnand-ecc-strength = <8>;\
+}}}' target/linux/ramips/dts/mt7621_jcg_q20.dts
